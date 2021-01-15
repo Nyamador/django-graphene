4b9b16fceef11cba6db7d4f365728f091c5063fe
Nyamador <dselasi4u@gmail.com>
2021-01-15 16:40:40 +0000

    Added mutation schema

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -5,6 +5,26 @@ from graphene_django import DjangoObjectType
 from .models import Link
 
 
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+
+        return CreateLink(
+            id=link.id,
+            url=link.url
+            description=link.description
+        )
+
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
